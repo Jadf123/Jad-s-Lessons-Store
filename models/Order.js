f67b59fbf1cd8b6cb5031cbd9045d22d81b0eb46
Jad Fakhoury <jfakhoury@outlook.com>
2024-12-10 12:56:22 +0400

    Delete models directory

--- models/Order.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const orderSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    phone: { type: String, required: true },
-    lessons: [{ type: mongoose.Schema.Types.ObjectId, ref: "Lesson" }],
-});
-
-module.exports = mongoose.model("Order", orderSchema);
